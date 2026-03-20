29a84b2e3f8d09ad2b0a52a29eaf687efe5dbb39
SARAN-28 <saranponnusamy2853@gmail.com>
2026-03-20 12:09:40 +0530

    added resend package

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,19 +1,10 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
         to,
         subject,
         html
